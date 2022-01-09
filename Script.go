92d58c605c66727b074898fe82a88ee6d4732db2
Tomas Alejandro <39777824+talejandro@users.noreply.github.com>
2022-01-08 21:10:13 -0300

    Initial commit

---
+++ Script.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func welcome() {
+	fmt.Println("Welcome to the Minecraft Server setup wizard")
+	time.Sleep(1 * time.Second)
+	fmt.Println("By Talejandro")
+	time.Sleep(1 * time.Second)
+
+}
+func main() {
+	welcome()
+
+}
