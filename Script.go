b9d0105fa7e9caf04a55a0b9a813007242e3ea26
Tomas Alejandro <39777824+talejandro@users.noreply.github.com>
2022-01-09 13:11:12 -0300

    Funcion Clear

--- Script.go
+++ Script.go
@@ -2,16 +2,35 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 	"time"
 )
 
 func welcome() {
+	//Bienvenida al Script
 	fmt.Println("Welcome to the Minecraft Server setup wizard")
 	time.Sleep(1 * time.Second)
 	fmt.Println("By Talejandro")
 	time.Sleep(1 * time.Second)
+	clear()
+}
+
+func clear() {
+	//Limpiar consola
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+func verification() {
+	//Verificacion del archivo
 
 }
+
+func change() {
+	//Cambiar Archivo Server.Propieties
+}
+
 func main() {
 	welcome()
 
